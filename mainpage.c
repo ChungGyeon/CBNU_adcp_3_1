f091df08b33eba1ee850f5d898e7936a5814d31b
ChungGyeon <imsdm@naver.com>
2024-04-16 20:43:10 +0900

    pj_1 : 화면 이니싱
    일반모드, 장문모드 만들면 함수 수정 필요
    
    최적화는 필요할려나
    
    딜레이를 넣고싶은데 직접 구현해야 보너스 점수가 있을거 같음
    
    Add code for printing my nae

---
+++ mainpage.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(){
+    system("clear"); //[issue]리눅스만 클리어됨, 윈도우 운영체제 대응하는거 필요
+    printf("------------------------------------\n");
+    for(int i=0; i<10; i++){
+        printf("\n");
+        if(i==4)printf("        뿌슝파슝 타자연습");
+    }
+    printf("------------------------------------\n");
+    printf("--------------모드 선택-------------\n");
+    printf(" 1.일반 타자 모드\n");
+    printf(" 2.장문 타자 모드\n");
+    printf(" 3.나가기\n");
+
+    int test=0; //입력받는 함수 숫자만 받게 함
+    int cnt=0; //여러번 오타 낸거 카운트용
+    while(1){
+        scanf("%d",&test);
+        if(test==1){printf("ㅋㅋ 아직 안만들었지롱\n");break;} //[issue]일반모드 만들면 수정
+        else if(test==2){printf("ㅋㅋ 아직 안만들었지롱\n");break;} //[issue]장문모드 만들면 수정
+        else if(test==3){printf("ㅋㅋ 다른거 눌러도 지금은 꺼지지롱\n");break;}
+        else{printf("1,2,3 중에서만 입력해주세요\n");cnt++;}
+        if(cnt>=10){printf("긴급탈출");break;}
+    }
+
+    return 0;
+}
+
+/* 나중에 모듈로 빼둘때
+int initiate_front(){
+
+    return 0;
+}
+*/
